bb95ff105a83a124d72fdaa60a77d25a54938b17
Christian Mühlethaler <christian.muehlethaler@optor.ch>
2019-07-25 15:47:56 +0200

    Erste Regel implementiert

--- js/mi_to_muiiu.js
+++ js/mi_to_muiiu.js
@@ -1,3 +1,7 @@
+const M = "M"
+const I = "I"
+const U = "U"
+
 /*
 Holt den selektierten Text
 TODO: Wenn kein Selektierten Text, dann die Aktion abbrechen
@@ -23,7 +27,7 @@ function add_schritt_to_html(kette, regel) {
     $("#history").append(
         "<div class='row'>" +
         "   <div class='col-6'>" + kette + "</div>" +
-        "   <div class='col-6'>" +regel +"</div>" +
+        "   <div class='col-6'>" + regel + "</div>" +
         "</div>"
     );
 }
@@ -38,22 +42,44 @@ function ersetze_text_der_aktuellen_kette(neue_kette) {
 $(document).ready(function() {
 
     /*
-    Handler für Regel 1
+    Handler für Regel 1: xI -> xIU
      */
     $("#regel_1").click(function() {
 
         var aktuelle_kette = $("#aktuelle_kette").text();
-
         var selectedText = getSelectedText();
 
-        var neue_kette = aktuelle_kette+selectedText;
+        //muss mit I enden
+        var lastChar = selectedText[selectedText.length -1];
+        if(lastChar != I){
+            alert("Der letzte Buchstabe der Selektion muss I sein. Die Selektion ist "+selectedText);
+            return;
+        }
 
+        // start- und Endpunkt des ersten Erscheinens
+        var start_index = aktuelle_kette.indexOf(selectedText);
+        var end_index = start_index+selectedText.length;
+
+        var erster_teil_kette = aktuelle_kette.substr(0, start_index+selectedText.length)
+        var neu = U
+        var zweiter_teil_kette = aktuelle_kette.substr(end_index)
+        var neue_kette = erster_teil_kette+neu+zweiter_teil_kette;
 
         ersetze_text_der_aktuellen_kette(neue_kette);
         add_schritt_to_html(aktuelle_kette, "Regel I");
 
     });
 
+    $("#regel_2").click(function() {
+        var aktuelle_kette = $("#aktuelle_kette").text();
+        var selectedText = getSelectedText();
+
+        ersetze_text_der_aktuellen_kette(neue_kette);
+        add_schritt_to_html(aktuelle_kette, "Regel II");
+
+    });
 
 
-});
+
+
+    });
